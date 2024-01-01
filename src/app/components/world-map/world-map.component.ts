bbaa62cc14f1619ba405634ab2f02199317d74ff
eldo88 <leegjones88@gmail.com>
2024-01-01 13:18:41 -0700

    API working with SVG map

--- src/app/components/world-map/world-map.component.ts
+++ src/app/components/world-map/world-map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WorldMapApiService } from '../../services/world-map-api.service';
 
 @Component({
   selector: 'app-world-map',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class WorldMapComponent {
 
+  countryData: any = {};
+
+  constructor(private api: WorldMapApiService) {}
+
+  setCountryData(event: any) {
+    this.api.getCountryData(event.target.id).subscribe(data => console.log(data));
+    this.api.setCountryData(event.target.id).subscribe((data: any) => 
+      this.countryData = {
+        ...data
+      });
+  }
 }
--- src/app/services/world-map-api.service.ts
+++ src/app/services/world-map-api.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorldMapApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCountryData(country: string) {
+    let apiUrl = `https://api.worldbank.org/v2/country/${country}?format=json`
+    return this.http.get(apiUrl);
+  }
+
+  setCountryData(country: string) {
+    console.log(country);
+    let subject = new Subject();
+
+    if (country === 'sj'){
+      subject.next({
+        errorMessage: 'Invalid parameter'
+      })
+    } else {
+        this.getCountryData(country).subscribe((data: any) => {
+          subject.next({
+            countryName: data[1][0].name,
+            capitalCity: data[1][0].capitalCity,
+            region: data[1][0].region.value,
+            latitude: data[1][0].latitude,
+            longitude: data[1][0].longitude,
+            incomeLevel: data[1][0].incomeLevel.value
+          })
+        }
+      );
+    }
+    return subject.asObservable();
+  }
 }
