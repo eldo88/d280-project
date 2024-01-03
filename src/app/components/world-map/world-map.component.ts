6132204b78a8abecf8fa444b137860aaf430dc3a
eldo88 <leegjones88@gmail.com>
2024-01-02 17:50:29 -0700

    Project works, need to add a little styling before turning in

--- src/app/components/world-map/world-map.component.ts
+++ src/app/components/world-map/world-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { WorldMapApiService } from '../../services/world-map-api.service';
 
 @Component({
@@ -10,36 +10,50 @@ export class WorldMapComponent {
 
   countryData: any = {};
 
-  constructor(private api: WorldMapApiService, private el: ElementRef) {}
+  constructor(private api: WorldMapApiService, private el: ElementRef, private cdr: ChangeDetectorRef) {}
+  
 
   setCountryData(event: any) {
-    this.api.getCountryData(event.target.id).subscribe(data => console.log(data));
-    this.api.setCountryData(event.target.id).subscribe((data: any) => 
-      this.countryData = {
-        ...data
-      });
+    this.api.setCountryData(event.target.id).subscribe((data: any) => {
+      this.countryData = { ...data };
+      this.cdr.detectChanges();
+    });
+  }
+
+  removeCountryData() {
+    this.countryData.name = '';
   }
 
   ngAfterViewInit(): void {
     const objectElement = this.el.nativeElement.querySelector('object');
+
     objectElement.addEventListener('load', () => {
+
       const svgDoc = objectElement.contentDocument;
       if (svgDoc) {
         const paths = svgDoc.querySelectorAll('path');
 
         paths.forEach((path: any) => {
+  
           path.addEventListener('mouseenter', () => {
-            console.log('a');
             path.style.fill = 'rgb(32, 222, 194)';
             path.style.stroke = 'blue';
-            path.classList.add('hovered');
+
+            const syntheticEvent = new MouseEvent('mouseenter', {
+              bubbles: true,
+              cancelable: true,
+              view: window
+            });
+
+            Object.defineProperty(syntheticEvent, 'target', { value: path, writable: false });
+
+            this.setCountryData(syntheticEvent);
           });
 
           path.addEventListener('mouseleave', () => {
-            console.log('b');
             path.style.fill = '';
             path.style.stroke = '';
-            path.classList.remove('hovered');
+            this.removeCountryData();
           });
         });
       }
